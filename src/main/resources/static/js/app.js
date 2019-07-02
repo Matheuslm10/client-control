8755c00c0ba30426f9d5fa74a0129a2cd0601bce
Matheus Machado <matheus.machado@azi.com.br>
2019-07-02 12:53:12 -0400

    parte 2 quase finalizada

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -4,17 +4,46 @@ appCliente.controller("indexController", function($scope, $http){
 
     $scope.nome = "Matheus";
     $scope.clientes=[];
+    $scope.cliente={};
 
-    $http({method:"GET", url:"http://localhost:8080/clientes"}).then(
-        function successCallback(response){
-            $scope.clientes=response.data;
+    $scope.carregarClientes= function (){
+        $http({method:"GET", url:"http://localhost:8080/clientes"}).then(
+            function successCallback(response){
+                $scope.clientes=response.data;
+            },
+            function errorCallback(response){
+                console.log(response.data);
+                console.log(response.status)
+            }
+        );
+    }
 
-            console.log(response.data);
-            console.log(response.status)
-        },
-        function errorCallback(response){
-            console.log(response.data);
-            console.log(response.status)
-        });
+    $scope.salvarClientes= function (){
+        $http({method:"POST", url:"http://localhost:8080/clientes", data: $scope.cliente}).then(
+            function successCallback(response){
+                $scope.clientes.push(response.data);
+            },
+            function errorCallback(response){
+                console.log(response.data);
+                console.log(response.status)
+            }
+        );
+    }
+
+    $scope.excluirCliente= function (cliente){
+        $http({method:"DELETE", url:"http://localhost:8080/clientes", data: $scope.cliente}).then(
+            function successCallback(response){
+                $scope.clientes.push(response.data);
+            },
+            function errorCallback(response){
+                console.log(response.data);
+                console.log(response.status)
+            }
+        );
+    }
+
+    $scope.carregarClientes();
+
+
 
 });
