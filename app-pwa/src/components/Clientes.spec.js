d11fee60414a40ea06b2802fd1a36d8a60b3365c
Matheus Machado <matheus.machado@azi.com.br>
2019-08-09 10:24:12 -0400

    Não consegui descobrir porque os testes não foram executados

---
+++ app-pwa/src/components/Clientes.spec.js
@@ -0,0 +1,33 @@
+import Clientes from './Clientes.vue'
+
+describe('Clientes.vue', () => {
+  test('setup correctly', () => {
+    expect(true).toBe(true)
+  })
+})
+
+// import { mount } from '@vue/test-utils'
+// import Clientes from './Clientes'
+
+// describe('Clientes', () => {
+//   const wrapper = mount(Clientes)
+
+//   console.log(wrapper.vm)
+
+  // it('renders the correct markup', () => {
+  //   expect(wrapper.html()).toContain('<span class="count">0</span>')
+  // })
+
+  // // it's also easy to check for the existence of elements
+  // it('has a button', () => {
+  //   expect(wrapper.contains('v-divider')).toBe(true)
+  // })
+
+
+  // it('button should increment the count', () => {
+  //   expect(wrapper.vm.count).toBe(0)
+  //   const button = wrapper.find('button')
+  //   button.trigger('click')
+  //   expect(wrapper.vm.count).toBe(1)
+  // })
+// })
